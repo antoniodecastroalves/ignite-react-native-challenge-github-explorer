2bc3c7038f9a1c335dc4d50af508899cf0727dea
“Antonio <antoniodecastroalves@gmail.com>
2022-08-18 21:35:07 -0300

    Challenge finished

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -37,22 +37,15 @@ export function Dashboard() {
   const { addRepository, repositories } = useRepositories();
 
   function handleAddRepository() {
-    /**
-     * TODO: 
-     * - call addRepository function sending inputText value;
-     * - clean inputText value.
-     */
+    addRepository(inputText)
+    inputRef.current?.blur();
+    setInputText('')
   }
 
   function handleRepositoryPageNavigation(id: number) {
-    /**
-     * TODO - navigate to the Repository screen sending repository id.
-     * Remember to use the correct prop name (repositoryId) to the repositoy id:
-     * 
-     * navigate(SCREEN NAME, {
-     *  repositoryId: id of the repository
-     * })
-     */
+    navigate('Repository', {
+      repositoryId: id
+    });
   }
 
   return (
@@ -66,11 +59,7 @@ export function Dashboard() {
               ref={inputRef}
               placeholder="Digite aqui 'usuário/repositório'"
               value={inputText}
-              /**
-               * TODO - update inputText value when input text value 
-               * changes:
-               * onChangeText={YOUR CODE HERE}
-               */
+              onChangeText={(value)=>setInputText(value)}
               onSubmitEditing={handleAddRepository}
               returnKeyType="send"
               autoCapitalize='none'
@@ -80,11 +69,7 @@ export function Dashboard() {
             <InputButton
               testID="input-button"
               onPress={handleAddRepository}
-              /**
-               * TODO - ensure to disable button when inputText is 
-               * empty (use disabled prop to this):
-               * disabled={CONDITION HERE}
-               */
+              disabled={inputText? false : true}
             >
               <Icon name="search" size={20} />
             </InputButton>
